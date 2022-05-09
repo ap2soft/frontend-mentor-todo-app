00e1b815643cbfff7f2ef14486af4ad4b759595f
Andrew Ponomarev <spargo32@gmail.com>
2022-05-09 22:54:57 +0300

    Initial Todo List seeding

---
+++ src/todoListManager.js
@@ -0,0 +1,18 @@
+export const initTodoList = () => {
+  if (localStorage.TodoAppInstalled !== undefined) {
+    return;
+  }
+
+  localStorage.TodoList = JSON.stringify([
+    {
+      id: 1,
+      name: "Complete online JavaScript course",
+      complete: true,
+    },
+    { id: 2, name: "Jog around the park 3x", complete: false },
+    { id: 3, name: "10 minutes meditation", complete: false },
+    { id: 4, name: "Read for 1 hour", complete: false },
+    { id: 5, name: "Pick up groceries", complete: false },
+    { id: 6, name: "Complete Todo App on Frontend Mentor", complete: false },
+  ]);
+};
