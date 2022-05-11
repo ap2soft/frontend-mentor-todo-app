4fba519b4f1e7ae806728c7abdc37544bea94872
Andrew Ponomarev <spargo32@gmail.com>
2022-05-11 08:54:14 +0300

    Desktop design WIP

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -31,21 +31,29 @@ const addNew = (newTodo) => {
 
 <template>
   <header
-    class="h-52 bg-opacity-50 bg-header bg-cover bg-center px-6 py-10 dark:bg-header-dark tablet:bg-header-desktop dark:tablet:bg-header-desktop-dark"
+    class="h-52 bg-opacity-50 bg-header bg-cover bg-center py-10 dark:bg-header-dark tablet:bg-header-desktop dark:tablet:bg-header-desktop-dark"
   >
-    <div class="flex items-center justify-between">
-      <h1 class="text-2xl font-bold tracking-[0.4rem] text-white">TODO</h1>
+    <div
+      class="mx-auto flex items-center justify-between px-6 tablet:max-w-md tablet:px-0"
+    >
+      <h1
+        class="text-2xl font-bold tracking-[0.4rem] text-white tablet:text-3xl"
+      >
+        TODO
+      </h1>
       <DarkModeSwitch />
     </div>
 
-    <NewTodo class="mt-10" @submit="addNew" />
+    <div class="mx-auto px-6 tablet:max-w-md tablet:px-0">
+      <NewTodo class="mt-10" @submit="addNew" />
+    </div>
   </header>
-  <main class="-mt-8 px-6">
+  <main class="mx-auto -mt-8 px-6 tablet:max-w-md tablet:px-0">
     <TodoList :items="items" />
   </main>
-  <aside class="mt-4 px-6">
+  <aside class="mt-4 px-6 tablet:px-6">
     <div
-      class="flex items-center justify-center space-x-4 rounded-md bg-white py-4 dark:bg-gray-800"
+      class="mx-auto flex items-center justify-center space-x-4 rounded-md bg-white py-4 dark:bg-gray-800 tablet:max-w-md"
     >
       <button
         v-for="filter in filters"
